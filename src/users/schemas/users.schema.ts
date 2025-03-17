eaf96a7a6891bb743268e06e8cfe63bbfd7187a9
rafaellore <birdneon3@gmail.com>
2025-03-17 12:07:42 -0300

    feat(users): adds dto, models and schema for users

---
+++ src/users/schemas/users.schema.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
+import * as mongoose from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { NextFunction } from 'express';
+
+export const UsersSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+});
+
+UsersSchema.pre('save', async function (next: NextFunction) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+
+    this['password'] = await bcrypt.hash(this['password'], 10);
+  } catch (err) {
+    return next(err);
+  }
+});
